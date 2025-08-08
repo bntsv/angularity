c034fa56a3355112e1d0c349716598d452a70777
bntsv <you@example.com>
2025-08-08 10:55:43 +0300

    Add routing and todos page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        loadComponent: async () => {
+            return import('./home/home').then((m) => m.Home)
+        }
+    },
+    {
+        path: 'todos',
+        loadComponent: async () => {
+            return import('./todos/todos').then((m) => m.Todos)
+        }
+    }
+];
